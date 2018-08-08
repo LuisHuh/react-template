2d75b329524a0903d8fd57eaaeb957c6cb037d09
Luis Huh <lhuh@palaceresorts.com>
2018-08-08 07:28:53 -0500

    Template configurado

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,57 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-   template: './public/index.html',
-   filename: 'index.html',
-   inject: 'body'
+	template: './public/index.html',
+	filename: 'index.html',
+	inject: 'body'
 });
 
 const config = {
-   mode: 'development',
-   entry: './src/index.jsx',
-   output: {
-      path: path.resolve('dist'),
-      filename: 'bundle.js'
-   },
-   module: {
-      rules: [{
-            test: /.jsx$/,
-            use: {
-               loader: 'babel-loader'
-            }
-         },
-         {
-            test: /\.css$/,
-            use: [{
-                  loader: "style-loader"
-               },
-               {
-                  loader: "css-loader"
-               }
-            ]
-         },
-         {
-            test: /\.svg$/,
-            loader: 'svg-inline-loader'
-         }
-      ]
-   },
-   resolve: {
-      extensions: ['*', '.js', '.jsx', '.json', '.svg', '.css']
-   },
-   plugins: [HtmlWebpackPluginConfig]
+	mode: 'development',
+	entry: './src/index.jsx',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [{
+				test: /.jsx$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [{
+						loader: "style-loader"
+					},
+					{
+						loader: "css-loader"
+					}
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif|ico)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					'file-loader'
+				]
+			}
+		]
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'public')
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx', '.json', '.svg', '.css']
+	},
+	plugins: [HtmlWebpackPluginConfig]
 };
 
 module.exports = config;
